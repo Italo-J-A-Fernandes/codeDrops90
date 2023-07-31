d4c83b35e242036a11886ab33d27f86c31657e69
Italo Fernandes <italojafernandes@hotmail.com>
2023-07-31 17:17:29 -0300

    :hammer: Iniciando a rota de produtos, que será a rota responsavel por receber o arquivo, extrair/tratar os dados do arquivo e gravar em banco

--- src/routes.ts
+++ src/routes.ts
@@ -8,4 +8,13 @@ router.get("/", (request: Request, response: Response) => {
   return response.send("Aplicação ok!");
 });
 
+router.post(
+  "/products",
+  multerConfig.single("file"),
+  (request: Request, response: Response) => {
+    console.log(request.file?.buffer.toString("utf-8"));
+    return response.send("Arquivo enviado com sucesso!");
+  }
+);
+
 export { router };
