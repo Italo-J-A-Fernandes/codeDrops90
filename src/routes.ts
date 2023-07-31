11ec109a8669c858d8617ebc23dfc7a645871762
Italo Fernandes <italojafernandes@hotmail.com>
2023-07-31 17:15:54 -0300

    :hammer: Criando a rota raiz do projeto e retornando uma msg de ok, caso tudo esteja ok.

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
-import { Router } from "express";
+import { Request, Response, Router, request, response } from "express";
+import multer from "multer";
 
+const multerConfig = multer();
 const router = Router();
+
+router.get("/", (request: Request, response: Response) => {
+  return response.send("Aplicação ok!");
+});
 
 export { router };
